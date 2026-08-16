539d5b3493008587ecd0515eba64e2cbe2528006
talise <talise.mwanza@student.moringaschool.com>
2026-08-17 00:53:39 +0300

    update on home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
-import LandingPage from "./pages/LandingPage";
+import { Routes, Route, Link } from "react-router-dom";
+import {DogProvider} from "./context/DogContext.jsx";
+import {UserProvider} from "./context/UserContext.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Signup from "./pages/Signup.jsx";
+import Signin from "./pages/Signin.jsx";
+import AdoptionForm from "./pages/AdoptionForm.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
+import AddDog from "./pages/AddDog.jsx";
+import Home from "./pages/Home.jsx";
+import Dogs from "./pages/Dogs.jsx";
 
 
 function App() {
 
   return (
-    
-      <Routes>
-  <Route path="/" element={<LandingPage />} />
-</Routes>
-
-    
-  )
+    <DogProvider>
+      <UserProvider>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dogs" element={<Dogs />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/adoption-form" element={<AdoptionForm />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin/add-dog" element={<AddDog />} />
+          </Routes>
+          <Footer />
+        </div>
+      </UserProvider>
+    </DogProvider>
+  );
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const { currentUser, logout } = useUser();
   return (
-    <nav className="navbar">
-      <h2>Dog Adoption Portal</h2>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/add">Add Dog</Link></li>
-        <li><Link to="/products">View Dogs</Link></li>
-        <li><Link to="/search">Search</Link></li>
-      </ul>
+    <nav style={{padding: "10px" ,borderBottom: "1px solid gray"}}>
+      <link to="/">Home</link>{"| "}
+      <link to="/dogs">Browse Dogs</link>{"| "}
+
+      {!currentUser && (
+        <>
+         <link to="/signup">Sign Up</link>
+         {"| "}
+         <link to="/signin">Sign In</link>
+          </>
+      )}
+
+
+      {currentUser && (
+        <>
+          <span>Welcome, {currentUser.firstName}</span>
+          {" | "}
+          {currentUser.role === "admin" && (
+            <link to="/admin/add-dog">Add Dog</link>
+          )}
+          {currentUser.role === "admin" && "|"}
+          <button onClick={logout}>Logout</button>
+        </>
+      )}
     </nav>
   );
 }
